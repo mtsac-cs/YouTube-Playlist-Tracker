567cf24db1111c52bcd1c52ab2969d9365f810a5
nacho481 <nachitoatilano@gmail.com>
2020-11-21 20:20:22 -0800

    New Dialog Prompt

---
+++ GiveBack-Hackathon.Wpf/Windows/UserInput.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GiveBack_Hackathon.Wpf.Windows
+{
+    /// <summary>
+    /// Interaction logic for UserInput.xaml
+    /// </summary>
+    public partial class UserInput : Window
+    {
+        public UserInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
